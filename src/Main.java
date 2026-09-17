1852988aa641c97916372445f181cd68d551e4a6
goncalop13 <gma.pereira@campus.fct.unl.pt>
2026-09-17 18:05:06 +0100

    Correção da mensagem NO_CONTACTS_WITH_SAME_PHONE

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main {
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String CONTACTS_WITH_SAME_PHONE = "There are contacts that share phone numbers.";
-    public static final String NO_CONTACTS_WITH_SAME_PHONE = "All contacts have different phone numbers";
+    public static final String NO_CONTACTS_WITH_SAME_PHONE = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
